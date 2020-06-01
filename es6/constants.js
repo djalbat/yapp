1633edf6d44cb8f5c98f6d437e816c4a91ee8077
James Smith <james.smith@djalbat.com>
2020-06-01 16:03:33 +0100

    Removed the FiraCode related constants.

--- es6/constants.js
+++ es6/constants.js
@@ -2,5 +2,3 @@
 
 export const GUTTER_HEIGHT = "100000px";
 export const JAVASCRIPT_LANGUAGE = "javascript";
-export const FIRA_CODE_FONT_SIZE = "1.3rem";
-export const FIRA_CODE_LINE_HEIGHT = 20;
--- es6/mixin/font/firaCode.js
+++ es6/mixin/font/firaCode.js
@@ -1,11 +1,9 @@
 "use strict";
 
-import { FIRA_CODE_FONT_SIZE, FIRA_CODE_LINE_HEIGHT } from "../../constants";
-
 const firaCodeFontMixin = `
 
-  font-size: ${FIRA_CODE_FONT_SIZE};
-  line-height: ${FIRA_CODE_LINE_HEIGHT}px;
+  font-size: 13px;
+  line-height: 20px;
   font-family: "Fira Code", monospace;
   text-rendering: optimizeLegibility; /* Force ligatures for Webkit, Blink, Gecko */
   font-feature-settings: "calt" 1;  /* Enable ligatures for IE 10+, Edge */
