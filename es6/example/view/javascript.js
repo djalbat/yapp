7e1f30a3f4113d9873971e9903119562a9ba1adb
James Smith <james.smith@djalbat.com>
2020-06-07 09:13:02 +0100

    Put the JavaScript example content back.

--- es6/example/view/javascript.js
+++ es6/example/view/javascript.js
@@ -7,7 +7,30 @@ import JavaScriptInterpreter from "../../interpreter/javascript";
 export default class JavaScriptView extends View {
   Interpreter = JavaScriptInterpreter;
 
-  initialContent = ``;
+  initialContent = `"use strict";
+
+import "juxtapose";
+
+import { Body } from "easy";
+
+function simpleApp(selector) {
+  const body = new Body(selector);
+
+  body.mount(
+
+    <p>
+      A simple application.
+    </p>
+
+  );
+}
+
+export default withStyle(simpleApp)\`
+
+  color: #333;
+
+\`;
+`;
 
   static defaultProperties = {
     className: "javascript"
