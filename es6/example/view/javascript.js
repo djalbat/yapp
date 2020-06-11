6289d5376a5da8ecec4e06a646205c015c5e7beb
James Smith <james.smith@djalbat.com>
2020-06-11 18:31:04 +0100

    A tiny change to the JavaScript listing.

--- es6/example/view/javascript.js
+++ es6/example/view/javascript.js
@@ -11,6 +11,8 @@ export default class JavaScriptView extends View {
 
 import "juxtapose";
 
+import withStyle from "easy-with-style"; ///
+
 import { Body } from "easy";
 
 function simpleApplication(selector) {
