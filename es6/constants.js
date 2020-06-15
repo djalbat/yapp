429129e8f16017203779c50329734c52b48b3e9a
James Smith <james.smith@djalbat.com>
2020-06-15 14:08:08 +0100

    Added plain text support.

--- es6/constants.js
+++ es6/constants.js
@@ -1,11 +1,12 @@
 "use strict";
 
+export const XML_LANGUAGE = "xml";
+export const JSON_LANGUAGE = "json";
+export const JAVASCRIPT_LANGUAGE = "javascript";
+export const PLAIN_TEXT_LANGUAGE = "plain-text";
 export const TOP_SIDE = "top";
 export const LEFT_SIDE = "left";
 export const RIGHT_SIDE = "right";
 export const BOTTOM_SIDE = "bottom";
-export const XML_LANGUAGE = "xml";
 export const GUTTER_HEIGHT = "100000px";
-export const JSON_LANGUAGE = "json";
-export const JAVASCRIPT_LANGUAGE = "javascript";
 export const TEMPLATE_LITERAL_DELIMITER_CONTENT = "`";
--- es6/utilities/plugin.js
+++ es6/utilities/plugin.js
@@ -2,15 +2,17 @@
 
 import XMLPlugin from "../plugin/xml";
 import JSONPlugin from "../plugin/json";
+import PlainTextPlugin from "../plugin/plainText";
 import JavaScriptPlugin from "../plugin/javascript";
 
-import { XML_LANGUAGE, JSON_LANGUAGE } from "../constants";
+import { XML_LANGUAGE, JSON_LANGUAGE, PLAIN_TEXT_LANGUAGE } from "../constants";
 
 export function pluginFromLanguageAndPlugin(language, Plugin) {
   if (!Plugin) {
     switch (language) {
       case XML_LANGUAGE: Plugin = XMLPlugin; break;
       case JSON_LANGUAGE: Plugin = JSONPlugin; break;
+      case PLAIN_TEXT_LANGUAGE: Plugin = PlainTextPlugin; break;
 
       default :
         Plugin = JavaScriptPlugin;
