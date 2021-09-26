de457fc4a45e75d4948fb18f63eea96db5f76d7d
James Smith <james.smith@djalbat.com>
2021-09-26 14:39:36 +0100

    Removed a comment from one of the styles.

--- src/yapp.js
+++ src/yapp.js
@@ -285,8 +285,8 @@ export default withStyle(Yapp)`
     font-size: 13px;
     line-height: 20px;
     font-family: "Fira Code";
-    text-rendering: optimizeLegibility; /* Force ligatures for Webkit, Blink, Gecko */
-    font-feature-settings: "calt" 1;  /* Enable ligatures for IE 10+, Edge */
+    text-rendering: optimizeLegibility;
+    font-feature-settings: "calt" 1;
     
   }
 
