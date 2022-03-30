8c6c46a3718781ca690072fb5381830871dd750f
James Smith <james.smith@djalbat.com>
2022-03-30 18:27:37 +0100

    Adjusted the JavaScript BNF to use an ellipsis instead of an exclamation mark to signify look-ahead.

--- src/parser/javascript.js
+++ src/parser/javascript.js
@@ -70,7 +70,7 @@ const bnf = `
 
                                  |  "delete" expression ";"
 
-                                 |  expression! ";"
+                                 |  expression... ";"
 
                                  |  try ( ( catch* finally ) | catch+ )
 
