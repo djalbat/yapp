40a1fabfbb0f25699633a5d118abded64a466d6f
James Smith <james.smith@djalbat.com>
2022-07-10 14:48:45 +0100

    A small improvement to the JavaScript lexical entries.

--- src/lexer/javascript.js
+++ src/lexer/javascript.js
@@ -29,7 +29,7 @@ const entries = [
     "keyword": "^(?:import|export|default|var|let|const|function|class|extends|constructor|static|break|return|continue|if|else|switch|case|throw|try|catch|finally|instanceof|typeof|async|await|do|while|for|in|of|as|new|this|super|delete|undefined|null|true|false|void|meta|debugger)\\b"
   },
   {
-    "identifier": "^[a-zA-Z]+"
+    "identifier": "^[a-zA-Z_]+"
   },
   {
     "unassigned": "^[^\\s]+"
