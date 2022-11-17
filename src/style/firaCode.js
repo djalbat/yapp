693635b14d0862769463f4e77c464e8211e54013
James Smith <james.smith@djalbat.com>
2022-11-17 19:00:12 +0000

    Corrected the FiraCode style.

--- src/style/firaCode.js
+++ src/style/firaCode.js
@@ -5,7 +5,7 @@ const firaCodeStyle = (host) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Light.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Light.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Light.woff") format("woff");
     font-weight: 300;
     font-style: normal;
   }
@@ -13,7 +13,7 @@ const firaCodeStyle = (host) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Regular.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Regular.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Regular.woff") format("woff");
     font-weight: 400;
     font-style: normal;
   }
@@ -21,7 +21,7 @@ const firaCodeStyle = (host) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Medium.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Medium.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Medium.woff") format("woff");
     font-weight: 500;
     font-style: normal;
   }
@@ -29,7 +29,7 @@ const firaCodeStyle = (host) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Bold.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Bold.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Bold.woff") format("woff");
     font-weight: 700;
     font-style: normal;
   }
