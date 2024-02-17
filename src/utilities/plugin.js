4226c3d33d7948a0d25dfbb6d9240a771c43657d
James Smith <james.smith@djalbat.com>
2024-02-17 16:42:44 +0000

    Propagated.

--- src/utilities/plugin.js
+++ src/utilities/plugin.js
@@ -5,7 +5,7 @@ import JSONPlugin from "../plugin/json";
 import PlainTextPlugin from "../plugin/plainText";
 import JavaScriptPlugin from "../plugin/javascript";
 
-import { XML_LANGUAGE, JSON_LANGUAGE, PLAIN_TEXT_LANGUAGE, JAVASCRIPT_LANGUAGE } from "../languages";
+import { XML_LANGUAGE, JSON_LANGUAGE, PLAIN_TEXT_LANGUAGE } from "../languages";
 
 export function pluginFromProperties(properties) {
   let Plugin;
@@ -13,13 +13,17 @@ export function pluginFromProperties(properties) {
   ({ Plugin = null } = properties);
 
   if (Plugin === null) {
-    const { language = JAVASCRIPT_LANGUAGE } = properties;
+    const { language = null } = properties;
 
     switch (language) {
       case XML_LANGUAGE: Plugin = XMLPlugin; break;
       case JSON_LANGUAGE: Plugin = JSONPlugin; break;
       case PLAIN_TEXT_LANGUAGE: Plugin = PlainTextPlugin; break;
-      case JAVASCRIPT_LANGUAGE: Plugin = JavaScriptPlugin; break;
+
+      default:
+        Plugin = JavaScriptPlugin;
+
+        break;
     }
   }
 
